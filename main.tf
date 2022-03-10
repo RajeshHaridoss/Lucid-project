be2e18749e903158f5a2ee9bb1bbba6202ef1e6f
RajeshHaridoss <92581320+RajeshHaridoss@users.noreply.github.com>
2022-03-10 16:07:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = "true"
   enable_dns_support   = "true"
 
-  tags = { 
+  tags { 
      Name = "vpc for demo"    
   }
 }
@@ -31,7 +31,7 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.main.id}"
 
-  tags = { 
+  tags { 
      Name = "IGW for demo" 
   }  
 }
