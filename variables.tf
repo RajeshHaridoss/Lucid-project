deb3d3f81d5e2dafee0fe5875828cca3b7ea89ec
RajeshHaridoss <92581320+RajeshHaridoss@users.noreply.github.com>
2022-03-10 20:06:35 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -38,12 +38,12 @@ variable "instance_type" {
 
 variable "public_subnets_cidr" {
   type = list(string)
-  default = ["10.0.4.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets_cidr" {
   type = list(string)
-  default = ["10.0.1.0/24", "10.0.3.0/24"]
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 
